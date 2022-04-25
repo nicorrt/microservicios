2485b52396c531e2d8f07cc18b1a28d299d67f93
nicorrt <nicorrt123@gmail.com>
2022-04-25 14:27:35 +0200

    gateway

---
+++ micorservicios-gateway-service/src/test/java/com/anramirez/micorserviciosgatewayservice/MicorserviciosGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anramirez.micorserviciosgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicorserviciosGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
